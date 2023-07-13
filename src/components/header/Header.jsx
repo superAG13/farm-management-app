13e7b52524c15d7aa6340e3290ab3c990f00f92a
AG <antoni.golabek@gmail.com>
2023-07-13 23:13:28 +0200

    create header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,46 @@
+import "./header.css";
+import {AiFillHome} from "react-icons/ai";
+import {CiWheat} from "react-icons/ci";
+import {LuWheat} from "react-icons/lu";
+import {GiWheat} from "react-icons/gi";
+
+function Header() {
+  return (
+    <header className="main-header">
+      <div className="container">
+        <div className="logo-box">
+          <GiWheat size="24px" />
+          <a href="/pages/home" className="logo">
+            MyFarm.net
+          </a>
+        </div>
+        <nav className="main-nav">
+          <ul className="nav-list">
+            <li>
+              <a href="/pages/home" className="nav-link">
+                <AiFillHome size="24px" />
+              </a>
+            </li>
+            <li>
+              <a href="/pages/gospodarstwo" className="nav-link">
+                Gospodarstwo
+              </a>
+            </li>
+            <li>
+              <a href="/pages/maps" className="nav-link">
+                Mapy
+              </a>
+            </li>
+            <li>
+              <a href="/pages/calendar" className="nav-link">
+                Kalendarz
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
