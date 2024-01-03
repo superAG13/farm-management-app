1a06ba29ad15cfa67b6d0e192dc98a2f23c2dcd6
AG <antoni.golabek@gmail.com>
2024-01-04 00:31:42 +0100

    edit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/farm-management-app/",
   plugins: [react()],
   server: {
     proxy: {
