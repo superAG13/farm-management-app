1bd79ff3fe3132df8b62c25ee1b239dca728d7ef
AG <antoni.golabek@gmail.com>
2024-01-04 23:29:10 +0100

    edit db

--- backend/db/database.js
+++ backend/db/database.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql");
 
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Dominik@1",
+//   database: "farm",
+// });
+
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Dominik@1",
-  database: "farm",
+  host: "myfarmdb.c5uc0208cp1t.eu-north-1.rds.amazonaws.com",
+  user: "admin",
+  password: "Dominika1",
+  database: "myfarmdb",
 });
 
 connection.connect((err) => {
